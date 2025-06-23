4824e88bd52e9d58a996fdfe758880b8073616e3
Ahmad Naufal <naufalopal002@gmail.com>
2025-06-23 23:36:22 +0700

    feat: dev and prod setup

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes.js');
 
+const environment = process.env.NODE_ENV || 'dev';
+
+// 2. Muat file .env yang sesuai dengan lingkungan
+require('dotenv').config({
+  path: `.env.${environment}`,
+});
+
 const init = async () => {
+  const host = process.env.HOST || 'localhost';
+  const port = process.env.PORT || 9000;
   const server = Hapi.server({
-    port: 9000,
-    host: 'localhost',
+    port: port,
+    host: host,
     routes: {
       cors:{
         origin: ['*'],
@@ -13,7 +22,7 @@ const init = async () => {
   });
   server.route(routes);
   await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`(${environment}) Server berjalan pada ${server.info.uri}`);
 };
 
 init();
